119e4ce45345ad87d0d70f347c85ea737819a789
Giuseppe0197 <faro.g@hotmail.it>
2021-10-20 14:58:43 +0200

    password generator

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+/* generazione della password utente */
+
+
+/* chedi all'utente il suo nome */
+
+let nome = prompt("Inserisci il tuo nome");
+console.log(nome);
+
+/* chiedi all'utente il suo cognome */
+
+let cognome = prompt("Inserisci il tuo cognome")
+console.log(cognome);
+
+/* chiedi all'utente il suo colore preferito */
+
+let colore = prompt("Inserisci il tuo colore preferito")
+console.log(colore);
+
+/* password generata */
+
+document.getElementById("password_utente").innerHTML = "La tua password è: " + nome + cognome + colore + 21;
+console.log("La tua password è: " + nome + cognome + colore + 21);
